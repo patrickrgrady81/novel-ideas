ce3aa9dfb887905f5e04731231903f71ac674cb6
Patrick Grady <patickrgrady81@gmail.com>
2020-03-20 14:15:25 -0400

    made scraper for a search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,4 +17,31 @@ class PagesController < ApplicationController
   def top100
     @books = Book.all
   end
+
+  def search_for(term)
+    # term = 'Stephen King'
+    url = "https://www.goodreads.com/search?utf8=%E2%9C%93&q=#{term}&search_type=books"
+    uri = URI.open(url)
+    doc = Nokogiri::HTML(uri)
+    book_titles = doc.css('.bookTitle span').collect do |title|
+      title.text
+    end
+    book_authors = doc.css('.authorName span').collect do |author|
+      author.text
+    end
+    book_urls = doc.css('.bookCover').collect do |ur|
+      ur.attribute('src')
+    end
+
+    book_titles.count.times do |i|
+      title = book_titles[i]
+      author = book_authors[i]
+      img = book_urls[i]
+      info = {title: title, author: author, img: img}
+      Book.new(info)
+    end
+
+
+    return Book.all
+  end
 end
